0411541c331787b2306359702e2253ea25db6f32
Naoki Saito <saito@alpha.local>
2015-05-14 23:41:42 +0900

    fix sha1 mismatch of arm-none-eabi-gcc48.rb

--- arm-none-eabi-gcc48.rb
+++ arm-none-eabi-gcc48.rb
@@ -4,7 +4,7 @@ class ArmNoneEabiGcc48 < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
   version '20140930'
   url 'https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q3-update/+download/gcc-arm-none-eabi-4_8-2014q3-20140805-mac.tar.bz2'
-  sha1 'da07fd4edc09da8748b3a61252eed793059c138f'
+  sha1 'd7e8b178fa744b78f6d75a2adfb9b7f554c1a158'
   keg_only "To use this package, other gcc compiler packages for ARM processor should be unlinked so that this package can be linked."
 
   def install 
