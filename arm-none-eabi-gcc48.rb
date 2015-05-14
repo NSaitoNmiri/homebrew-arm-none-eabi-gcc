68d2e2c5e86669c1cf240eff42d82e5877ab25c6
Naoki Saito <saito@alpha.local>
2015-05-14 23:35:22 +0900

    update arm-none-eabi-gcc48 to 2014-q3

--- arm-none-eabi-gcc48.rb
+++ arm-none-eabi-gcc48.rb
@@ -2,10 +2,9 @@ require 'formula'
 
 class ArmNoneEabiGcc48 < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
-  version '20140609'
-  url 'https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q2-update/+download/gcc-arm-none-eabi-4_8-2014q2-20140609-mac.tar.bz2'
-  sha1 '751895b7c967dcc964b98c86f06198a3457a859e'
-
+  version '20140930'
+  url 'https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q3-update/+download/gcc-arm-none-eabi-4_8-2014q3-20140805-mac.tar.bz2'
+  sha1 'da07fd4edc09da8748b3a61252eed793059c138f'
   keg_only "To use this package, other gcc compiler packages for ARM processor should be unlinked so that this package can be linked."
 
   def install 
